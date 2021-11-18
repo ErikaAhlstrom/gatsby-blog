8560f8d30aa5525f728df3db3f07885ac86809de
“erikaahlstrom” <info@erikasoldh.se>
2021-11-18 13:52:47 +0100

    Set up basic about page.

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+
+
+const About = () => {
+    return (
+        <Layout>
+            <SEO title="Contact"/>
+            <h1>About Page</h1>
+        </Layout>
+    )
+}
+
+export default About
