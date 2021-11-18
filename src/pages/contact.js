370e34dcedb4589e297e6fe823f54516ee57ac5c
“erikaahlstrom” <info@erikasoldh.se>
2021-11-18 13:52:24 +0100

    Set up basic contact page.

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+
+const Contact = () => {
+    return (
+        <Layout>
+            <SEO title="Contact"/>
+            <h1>Contact Page</h1>
+        </Layout>
+    )
+}
+
+export default Contact
