4b375962db147bd7b40e04b63c4301dfe09d7e8c
“erikaahlstrom” <info@erikasoldh.se>
2021-11-18 15:46:20 +0100

    Add collapsable navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
+    const [show, setShow] = useState(false)
     return (
-        <nav>
-            <ul>
-                <Link to="/">
-                    <li>Home</li>
-                </Link>
-                <Link to="/about">
-                    <li>About</li>
-                </Link>
-                <Link to="/contact">
-                    <li>Contact</li>
-                </Link>
-                <Link to="/blog">
-                    <li>Blog</li>
-                </Link>
-            </ul>
+        <nav className="navbar">
+            <div className="nav-center">
+
+                <div className="nav-header">
+                    <Link to="/">
+                        <img src={logo} alt="simply recipes" />
+                    </Link>
+                    <div className="nav-btn" onClick={() => setShow(!show)}>
+                        <GiHamburgerMenu />
+                    </div>
+                </div>
+                <div className={show ? "nav-links show-links" : "nav-links"}>
+                    <Link to="/" className="nav-link" activeClassName="active-link" onClick={() => setShow(!show)}>
+                        home
+                    </Link>
+                    <Link to="/blog" className="nav-link" activeClassName="active-link" onClick={() => setShow(!show)}>
+                        blog
+                    </Link>
+                   {/*  <Link to="/tags" className="nav-link" activeClassName="active-link" onClick={() => setShow(!show)}>
+                        tags
+                    </Link> */}
+                    <Link to="/about" className="nav-link" activeClassName="active-link" onClick={() => setShow(!show)}>
+                        about
+                    </Link>
+                    <Link to="/contact" className="nav-link" activeClassName="active-link" onClick={() => setShow(!show)}>
+                        contact
+                    </Link>
+                    
+                </div>
+            </div>
         </nav>
     )
 }
