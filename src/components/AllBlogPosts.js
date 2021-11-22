3dba1d35ef671e936019b7c03e6625156bbc777d
“erikaahlstrom” <info@erikasoldh.se>
2021-11-22 12:47:02 +0100

    Create component to handle all blog posts.

---
+++ src/components/AllBlogPosts.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import BlogPostsList from './BlogPostsList'
+import TagsList from './TagsList'
+
+const query = graphql`
+  {
+    allContentfulSimpleBlogPost(sort: {fields: postedAt, order: DESC}) {
+      nodes {
+        author
+        title
+        tags {
+          tags
+        }
+        postedAt(formatString: "DD MMMM, YYYY")
+        image {
+          gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+        id
+      }
+    }
+  }
+`
+
+const AllBlogPosts = () => {
+    const {
+        allContentfulSimpleBlogPost: {nodes: blogPosts}
+    } = useStaticQuery(query)
+    return (
+        <div>
+            <TagsList blogPosts={blogPosts}/>
+            <BlogPostsList blogPosts={blogPosts}/>
+        </div>
+    )
+}
+
+export default AllBlogPosts
+
