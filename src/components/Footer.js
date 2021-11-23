a6e96ad6471129805ff7e1dcba5a6de187cef34b
“erikaahlstrom” <info@erikasoldh.se>
2021-11-23 13:25:42 +0100

    Add html structure and styling to Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="page-footer">
-            <p>&copy; {new Date().getFullYear()} 
-                <span> Simply A Blog</span>. Built with {" "} <a href="http://gatsby.com">Gatsby js</a> </p>
+            <p>&copy; {new Date().getFullYear()} Erika Ahlström </p>
+            <p>
+                <span> A bog for plant lovers</span>. Built with {" "} <a href="http://gatsby.com">Gatsby js.</a> 
+            </p>
         </footer>
     )
 }
