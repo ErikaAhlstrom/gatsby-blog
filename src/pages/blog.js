580fcbcbd6afc015a71b0078fdee2a0562a2126d
“erikaahlstrom” <info@erikasoldh.se>
2021-11-23 18:38:20 +0100

    Add mainBlogList component.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,44 @@
 import React from 'react'
-import AllBlogPosts from '../components/AllBlogPosts'
+import MainBlogPostList from '../components/MainBlogPostList'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import TagsList from '../components/TagsList'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 
 const Blog = () => {
+    const {
+        allContentfulSimpleBlogPost: {nodes: blogPosts}
+    } = useStaticQuery(query)
     return (
         <Layout>
             <SEO title="Blog"/>
             <main className="page">
-                <AllBlogPosts />
+              <section className="blog-page">
+                <TagsList blogPosts={blogPosts}/>
+                <MainBlogPostList blogPosts={blogPosts}/>
+              </section>
             </main>
         </Layout>
     )
 }
+
+export const query = graphql`
+  {
+    allContentfulSimpleBlogPost(sort: {fields: postedAt, order: DESC}) {
+      nodes {
+        author
+        title
+        tags {
+          tags
+        }
+        postedAt(formatString: "DD MMMM, YYYY")
+        image {
+          gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+        id
+      }
+    }
+  }
+`
 
 export default Blog
