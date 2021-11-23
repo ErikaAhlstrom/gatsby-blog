37f2c817fbae38a139faf7f5a7617d49ae061fc3
“erikaahlstrom” <info@erikasoldh.se>
2021-11-23 13:26:38 +0100

    Add html structure and styling to About Page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
-
+import { StaticImage } from "gatsby-plugin-image"
+import AllBlogPosts from "../components/AllBlogPosts"
 
 const About = () => {
     return (
         <Layout>
             <SEO title="Contact"/>
             <main className="page">
-                <h1>About Page</h1>
+                <section className="contact-page">
+                    <StaticImage
+                        src="../assets/images/about.jpg"
+                        alt="Leaves"
+                        className="hero-img"
+                        placeholder="traced-svg"
+                        layout="fullWidth">
+                    </StaticImage>
+                    <article>
+                        <h2 className="special-header">Take a breather!</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat iusto aliquid officiis libero velit dicta, assumenda, odio mollitia voluptatibus dolorum alias ut recusandae! Recusandae nostrum modi assumenda voluptatum deleniti dolore.</p>
+                        <p>Fugiat iusto aliquid officiis libero velit dicta, assumenda, odio mollitia voluptatibus dolorum alias ut recusandae! Recusandae nostrum modi assumenda voluptatum deleniti dolore.</p>
+                    </article>
+                    
+                </section>
 
             </main>
         </Layout>
