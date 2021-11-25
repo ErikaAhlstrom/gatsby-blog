7be8c118fa80ff01750a64cb8486b3c0debf218b
“erikaahlstrom” <info@erikasoldh.se>
2021-11-25 17:08:33 +0100

    Change from h2 to h1.

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,7 +22,7 @@ const About = ( {data} ) => {
                     </StaticImage>          
                   <div>
                     <article>
-                        <h2 className="special-header">Take a breather!</h2>
+                        <h1>Take a breather!</h1>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat iusto aliquid officiis libero velit dicta, assumenda, odio mollitia voluptatibus dolorum alias ut recusandae! Recusandae nostrum modi assumenda voluptatum deleniti dolore.</p>
                         <p>Fugiat iusto aliquid officiis libero velit dicta, assumenda, odio mollitia voluptatibus dolorum alias ut recusandae! Recusandae nostrum modi assumenda voluptatum deleniti dolore.</p>
                     </article>
