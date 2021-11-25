de520dd6bfdf5e7298458a3dcf2b19c36ec8d7c2
“erikaahlstrom” <info@erikasoldh.se>
2021-11-25 12:29:55 +0100

    Change position for social media bar.

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,20 +12,22 @@ const About = ( {data} ) => {
             <SEO title="Contact"/>
             <main className="page">
                 <section className="layout-page">
+                  
                     <StaticImage
                         src="../assets/images/about.jpg"
                         alt="Leaves"
                         className="hero-img"
                         placeholder="traced-svg"
                         layout="fullWidth">
-                    </StaticImage>
-                    <SocialMediaBar position="center"/>
+                    </StaticImage>          
+                  <div>
                     <article>
                         <h2 className="special-header">Take a breather!</h2>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat iusto aliquid officiis libero velit dicta, assumenda, odio mollitia voluptatibus dolorum alias ut recusandae! Recusandae nostrum modi assumenda voluptatum deleniti dolore.</p>
                         <p>Fugiat iusto aliquid officiis libero velit dicta, assumenda, odio mollitia voluptatibus dolorum alias ut recusandae! Recusandae nostrum modi assumenda voluptatum deleniti dolore.</p>
                     </article>
-                    
+                    <SocialMediaBar position="center"/>
+                  </div>
                 </section>
                 <FeaturedBlogPosts />
             </main>
